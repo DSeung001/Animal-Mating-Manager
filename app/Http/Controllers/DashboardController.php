42d6bd2bad1f9a1386dec9f2b89fbc33a95f4c08
지승렬 <seungryeol156@gmail.com>
2022-12-15 09:37:42 +0900

    Implementing population charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Egg;
 use App\Models\Mating;
+use App\Models\Reptile;
+use Carbon\Carbon;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -11,11 +13,13 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
     private Egg $egg;
+    private Reptile $reptile;
     private Mating $mating;
 
-    public function __construct(Egg $egg, Mating $mating){
+    public function __construct(Egg $egg, Mating $mating, Reptile $reptile){
         $this->egg = $egg;
         $this->mating = $mating;
+        $this->reptile = $reptile;
     }
 
     public function dashboard()
@@ -41,6 +45,77 @@ class DashboardController extends Controller
            ];
         }, $eggs);
 
-        return view('dashboard', compact('eggs'));
+
+        /*
+         * 수 개체들 전체
+         *
+         *  => 확대해서 보기 http://jsfiddle.net/fj6d2/3025/
+         * 1. 월 카테고리
+         * */
+
+        /*
+         * 개체수 증가량은 => created_at을 통해 구분 가능
+         * min created_at, max created_at을 가져온 후
+         * */
+
+        $graphCategories = [];
+        $allReptileList = [];
+        $maleReptileList =[];
+        $femaleReptileList = [];
+        $undefinedReptileList = [];
+
+        if($this->reptile->where('id', '!=', '0')->first() !== null){
+            $maxCreatedAt = Carbon::parse($this->reptile
+                ->select(DB::raw('max(created_at) as created_at'))
+                ->where('user_id', Auth::id())
+                ->first()['created_at']);
+            $minCreatedAt = Carbon::parse($this->reptile
+                ->select(DB::raw('min(created_at) as created_at'))
+                ->where('user_id', Auth::id())
+                ->first()['created_at']);
+            $diffMonth = $minCreatedAt->diffInMonths($maxCreatedAt)+1;
+
+            for($re = 0 ; $re <= $diffMonth ; $re++){
+                $graphCategories[] = $minCreatedAt->format('Y.m');
+
+                $maleCount = $this->reptile
+                    ->select(DB::raw('count(id) as count'))
+                    ->where('created_at', 'like', $minCreatedAt->format('Y-m')."%")
+                    ->where('user_id', Auth::id())
+                    ->conditionGender('m')
+                    ->first()['count'];
+
+                $femaleCount = $this->reptile
+                    ->select(DB::raw('count(id) as count'))
+                    ->where('created_at', 'like', $minCreatedAt->format('Y-m')."%")
+                    ->where('user_id', Auth::id())
+                    ->conditionGender('f')
+                    ->first()['count'];
+
+                $undefinedCount = $this->reptile
+                    ->select(DB::raw('count(id) as count'))
+                    ->where('created_at', 'like', $minCreatedAt->format('Y-m')."%")
+                    ->where('user_id', Auth::id())
+                    ->conditionGender()
+                    ->first()['count'];
+
+                $allCount = $maleCount + $femaleCount + $undefinedCount;
+
+                if($re > 0){
+                    $allReptileList[] = $allReptileList[$re-1] + $allCount;
+                    $maleReptileList[] = $maleReptileList[$re-1] + $maleCount;
+                    $femaleReptileList[] = $femaleReptileList[$re-1] + $femaleCount;
+                    $undefinedReptileList[] = $undefinedReptileList[$re-1] + $undefinedCount;
+                } else {
+                    $allReptileList[] = $allCount;
+                    $maleReptileList[] = $maleCount;
+                    $femaleReptileList[] = $femaleCount;
+                    $undefinedReptileList[] = $undefinedCount;
+                }
+                $minCreatedAt->addMonth();
+            }
+        }
+
+        return view('dashboard', compact('eggs', 'graphCategories', 'allReptileList', 'maleReptileList', 'femaleReptileList', 'undefinedReptileList'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
     @push('styles')
         <link href='{{asset('style/fullcalendar-min.css')}}' rel='stylesheet'/>
         <link href='{{asset('style/fullcalendar-custom.css')}}' rel='stylesheet'/>
+        <link href='{{asset('style/highchart-custom.css')}}' rel='stylesheet'/>
         <script src='{{asset('js/fullcalendar-min.js')}}'></script>
     @endpush
 
@@ -11,16 +12,21 @@
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-10 max-w-[1280px]">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6 max-w-[1280px]">
-            <p class="">
-                부화 예상일
-            </p>
-            <div id='calendar'>
+            <p>부화 예상일</p>
+            <div id='calendar' class="mb-6"></div>
 
-            </div>
+            <figure class="highcharts-figure ">
+                <div id="container"></div>
+            </figure>
         </div>
     </div>
 
         @push('scripts')
+            <script src="https://code.highcharts.com/highcharts.js"></script>
+            <script src="https://code.highcharts.com/modules/series-label.js"></script>
+            <script src="https://code.highcharts.com/modules/exporting.js"></script>
+            <script src="https://code.highcharts.com/modules/export-data.js"></script>
+            <script src="https://code.highcharts.com/modules/accessibility.js"></script>
             <script>
                 let events = {!! json_encode($eggs) !!};
 
@@ -43,6 +49,59 @@
                         height:600
                     });
                     calendar.render();
+                });
+
+
+                Highcharts.chart('container', {
+                    title: {
+                        text: '현재 개체수 그래프',
+                        align: 'left'
+                    },
+                    yAxis: {
+                        title: {
+                            text: '개체수'
+                        }
+                    },
+
+                    xAxis: {
+                        categories: {!! json_encode($graphCategories) !!}
+                    },
+
+                    plotOptions: {
+                        line: {
+                            dataLabels: {
+                                enabled: true
+                            },
+                            enableMouseTracking: false
+                        }
+                    },
+                    series: [{
+                        name: '모든 개체',
+                        data: {!! json_encode($allReptileList) !!}
+                    },{
+                        name: '수컷 개체',
+                        data: {!! json_encode($maleReptileList) !!}
+                    },{
+                        name: '암컷 개체',
+                        data: {!! json_encode($femaleReptileList) !!}
+                    },{
+                        name: '미구분 개체',
+                        data: {!! json_encode($undefinedReptileList) !!}
+                    }],
+                    responsive: {
+                        rules: [{
+                            condition: {
+                                maxWidth: 500
+                            },
+                            chartOptions: {
+                                legend: {
+                                    layout: 'horizontal',
+                                    align: 'center',
+                                    verticalAlign: 'bottom'
+                                }
+                            }
+                        }]
+                    }
                 });
             </script>
         @endpush
